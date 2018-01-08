7182afadfff3c850494e6776cbca6ceec36f7e8e
rahul-fakir <rahul.fakir@gmail.com>
2018-01-09 01:55:31 +0200

    Added Login Activity

--- app/src/main/java/com/pear/rahulfakir/pear_android/WelcomeActivity.java
+++ app/src/main/java/com/pear/rahulfakir/pear_android/WelcomeActivity.java
@@ -8,20 +8,30 @@ import android.widget.Button;
 
 public class WelcomeActivity extends AppCompatActivity {
     //  Components
-    private Button btnWelcomeActivity_Register;
+    private Button WelcomeActivity_btnRegister;
+    private Button WelcomeActivity_btnLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
 
-        btnWelcomeActivity_Register = (Button) findViewById(R.id.btnWelcomeActivity_Register);
+        WelcomeActivity_btnRegister = (Button) findViewById(R.id.WelcomeActivity_btnRegister);
+        WelcomeActivity_btnLogin = (Button) findViewById(R.id.WelcomeActivity_btnLogin);
 
-        btnWelcomeActivity_Register.setOnClickListener(new View.OnClickListener() {
+        WelcomeActivity_btnRegister.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent toRegisterActivityIntent = new Intent(getApplicationContext(), RegistrationActivity.class);
                 startActivity(toRegisterActivityIntent);
+            }
+        });
+
+        WelcomeActivity_btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent toLoginActivity = new Intent(getApplicationContext(), LoginActivity.class);
+                startActivity(toLoginActivity);
             }
         });
     }
